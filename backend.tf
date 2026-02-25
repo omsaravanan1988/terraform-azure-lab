5791cbb7cc81f54410ad09789bf0fa2663f20b68
omsaravanan1988 <omsaravanan1988@gmail.com>
2026-02-25 15:13:47 +0000

    removed backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "example-resources"
-    storage_account_name = "labtfremotebackendsa "
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
